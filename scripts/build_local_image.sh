7e945031c7797460640e6828240f9eb1c24f6b86
m-sef <seth.l.moore0506@gmail.com>
2026-06-25 14:48:57 -0400

    Added *.tar to .gitignore, modified scripts/build_local_image.sh to use correct name (mutilate), added nodeName to yaml/mutilate_leader.yaml for testing purposes

--- scripts/build_local_image.sh
+++ scripts/build_local_image.sh
@@ -2,9 +2,9 @@
 # Author(s): m-sef (https://github.com/m-sef)
 # WARNING - Relies on deprecated tools!
 
-sudo docker build -t webserver .
-sudo docker save -o webserver.tar webserver:latest
-sudo ctr -n=k8s.io images import webserver.tar
-sudo ctr -n=k8s.io image list | grep webserver
+sudo docker build -t mutilate .
+sudo docker save -o mutilate.tar mutilate:latest
+sudo ctr -n=k8s.io images import mutilate.tar
+sudo ctr -n=k8s.io image list | grep mutilate
 
 exit 0
