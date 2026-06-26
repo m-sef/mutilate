0e4189cadb99adc9439cefb2d54d1e9cd270b5d2
m-sef <seth.l.moore0506@gmail.com>
2026-06-26 13:59:15 -0400

    benchmark

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
 run_benchmark() {
+    UPDATE=$1
+    QPS=$2
+
     sudo kubectl apply -Rf yaml/
     sudo kubectl wait --for=condition=Ready pod --all -n memcached --timeout=120s
     sudo kubectl wait --for=condition=Ready pod --all -n mutilate --timeout=120s
 
-    sudo kubectl exec -n mutilate mutilate-leader-0 -- ./scripts/mutilate_leader/run_workload.sh $1 $2
+    sudo kubectl exec -n mutilate mutilate-leader-0 -- ./scripts/mutilate_leader/run_workload.sh $1 $2 >> benchmark/$UPDATE\_$QPS.log
 
     sudo kubectl delete -Rf yaml/
 }
 
-run_benchmark 0.25 20000
+rm -rf benchmark/*
 
-#for i in 20000 40000 60000 80000 100000 120000 140000 160000 180000 200000 220000 240000 260000 280000 300000 320000 340000 360000 380000 400000; do
-#    run_benchmark 0.25 $i
-#    sleep 1
-#done
+for i in 20000 40000 60000 80000 100000 120000 140000 160000 180000 200000 220000 240000 260000 280000 300000 320000 340000 360000 380000 400000; do
+    run_benchmark 0.25 $i
+    sleep 1
+done
 
 exit 0
