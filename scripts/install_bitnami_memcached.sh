3322abbf2e06d5fc7ec99bc18b60ca93e042e03c
Seth Moore <seth.l.moore0506@gmail.com>
2026-06-29 19:16:24 -0400

    Modified script

--- scripts/install_bitnami_memcached.sh
+++ scripts/install_bitnami_memcached.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install my-cache bitnami/memcached \
+sudo helm repo add bitnami https://charts.bitnami.com/bitnami
+sudo helm install my-cache bitnami/memcached \
   --set architecture="high-availability" \
-  --set autoscaling.replicas=2 \
+  --set replicaCount=2 \
   --set fullnameOverride=memcached-service \
   -n memcached \
   --create-namespace
