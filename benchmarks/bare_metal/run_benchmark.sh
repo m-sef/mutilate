e12826362f8afa0e8ead92dfd8d6e0a3cff4b3f9
slmoore <slmoore@master.slmoore-309005.bayopsys-pg0.wisc.cloudlab.us>
2026-06-29 18:44:36 +0000

    Results

--- benchmarks/bare_metal/run_benchmark.sh
+++ benchmarks/bare_metal/run_benchmark.sh
@@ -11,8 +11,8 @@ AGENT1=10.10.1.2
 AGENT2=10.10.1.3
 
 run_experiment() {
-        echo "START MEMCACHED 16 THREADS"
-        ssh $SERVER "taskset -c 0-15 memcached -p $PORT -u nobody -t 16 -m 32G -c 8192 -b 8192 -l $SERVER -B binary > mcd_11211.log 2>&1 < /dev/null &"
+        echo "START MEMCACHED 20 THREADS"
+        ssh $SERVER "taskset -c 0-15 memcached -p $PORT -u nobody -t 20 -m 32G -c 8192 -b 8192 -l $SERVER -B binary > mcd_11211.log 2>&1 < /dev/null &"
 
         echo "START LOAD GENERATION AGENTS"
         ssh $AGENT1 "mutilate --agentmode --threads=16 > agent.log 2>&1 < /dev/null &"
