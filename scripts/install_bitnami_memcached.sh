dd7a1438f5537b02670c7f7fc8d02415d65a5f0b
m-sef <seth.l.moore0506@gmail.com>
2026-06-30 09:51:38 -0400

    Revert "Attempt at making bitnami chart work"
    This reverts commit c1cd682c2d2815c607b3f035f88a220d3d38ab0b.

--- scripts/install_bitnami_memcached.sh
+++ scripts/install_bitnami_memcached.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
 sudo helm repo add bitnami https://charts.bitnami.com/bitnami
 sudo helm install my-cache bitnami/memcached \
   --set architecture="high-availability" \
   --set replicaCount=2 \
   --set fullnameOverride=memcached-service \
-  -f "$SCRIPT_DIR/bitnami-memcached-values.yaml" \
   -n memcached \
   --create-namespace
 
